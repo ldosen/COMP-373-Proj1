334825d6024e23297f68e3c9515bad3a3f468bf1
Luke Dosen <lddosen@gmail.com>
2019-02-23 14:50:00 -0600

    created a bare bones facility interface that will need to be modified/added to in the future

--- src/facility/Facility.java
+++ src/facility/Facility.java
@@ -1,5 +1,12 @@
 package facility;
 
-public interface Facility {
+import javafx.beans.binding.ObjectExpression;
 
+public interface Facility {
+    public Object listFacilities();
+    public Object getFacilityInformation();
+    public Object requestAvailableCapacity();
+    public Object addNewFacility();
+    public void addFacilityDetail();
+    public Object removeFacility();
 }
